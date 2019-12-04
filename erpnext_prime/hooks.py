bdd91600fee86f19add2aec936b136d438b9d480
Palash Jhabak <palash@techlift.in>
2019-12-04 12:55:25 +0000

    adding customer quick form address non mandatory

--- erpnext_prime/hooks.py
+++ erpnext_prime/hooks.py
@@ -17,6 +17,7 @@ app_license = "MIT"
 # include js, css files in header of desk.html
 # app_include_css = "/assets/erpnext_prime/css/erpnext_prime.css"
 # app_include_js = "/assets/erpnext_prime/js/erpnext_prime.js"
+app_include_js = "/assets/erpnext_prime/js/erpnext_prime.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/erpnext_prime/css/erpnext_prime.css"
