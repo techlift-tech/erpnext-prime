9d67db29c502f448f25135c892f5526efb4bf8a8
Palash Jhabak <palash@techlift.in>
2020-05-21 16:43:20 +0530

    Add MRP field in Stock Entry Form

--- erpnext_prime/hooks.py
+++ erpnext_prime/hooks.py
@@ -29,6 +29,7 @@ page_js = {"point-of-sale" : "public/js/point_of_sale.js"}
 
 # include js in doctype views
 # doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_js = {"Stock Entry" : "public/js/stock_entry.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
