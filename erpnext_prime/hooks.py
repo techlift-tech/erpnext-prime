3f1d4e001241bc335d582a0dedaaf08b305edc09
Palash Jhabak <palash@techlift.in>
2020-06-17 06:35:15 +0530

    Adding MRP to PO

--- erpnext_prime/hooks.py
+++ erpnext_prime/hooks.py
@@ -31,7 +31,8 @@ page_js = {"point-of-sale" : "public/js/point_of_sale.js"}
 # doctype_js = {"doctype" : "public/js/doctype.js"}
 doctype_js = {
         "Stock Entry" : "public/js/stock_entry.js",
-        "Item Price": "public/js/item_price.js"
+        "Item Price": "public/js/item_price.js",
+        "Purchase Order": "public/js/purchase_order.js"
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
---
+++ erpnext_prime/public/js/purchase_order.js
@@ -0,0 +1,22 @@
+frappe.ui.form.on('Purchase Order Item', {
+	item_code(frm, cdt, cdn){
+		var purchase_order_entry_doc = frappe.get_doc(cdt, cdn)
+		if(purchase_order_entry_doc){
+			var item_code = purchase_order_entry_doc.item_code
+			var item_prices = frappe.db.get_list('Item Price', {
+				fields: ['price_list_rate'],
+				filters: {
+					item_code: item_code,
+					price_list: "MRP",
+					selling: 1
+				}
+			}).then(function(prices){
+				if(prices.length >= 1){
+					purchase_order_entry_doc.mrp = prices[0].price_list_rate
+					frm.refresh()
+				}
+			})
+		}
+
+	}
+})
